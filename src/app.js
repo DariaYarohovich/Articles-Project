ff0e53f66fbd05c24b0fb3af1aa2249fcc3520b9
DariaYarohovich <dashayarohovich@mail.ru>
2019-02-16 19:24:14 +0300

    Added Menu with children (MenuItem)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import ArticlesPage from './routes/articles';
 import CommentsPage from './routes/comments';
 import Select from './components/Select/Select';
 import Datepicker from './components/Datepicker/Datepicker';
 import Counter from './components/Counter';
 import ErrorPage from './routes/error';
+import Menu from './components/Menu';
+import MenuItem from './components/Menu/MenuItem';
 
 import PropTypes from 'prop-types';
 import { articleType } from './types';
@@ -20,20 +22,12 @@ class App extends Component {
                 <div className="app__header">
                     <div className="header container">
                         <Datepicker />
-                        <div className="navigation">
-                            <div className="navigation__item">
-                                <NavLink to={"/counter"} activeStyle={{ fontWeight: 700 }} >Counter</NavLink>
-                            </div>
-                            <div className="navigation__item">
-                                <NavLink to={"/filter"} activeStyle={{ fontWeight: 700 }}>Filter</NavLink>
-                            </div>
-                            <div className="navigation__item">
-                                <NavLink to={"/articles"} activeStyle={{ fontWeight: 700 }}>Articles</NavLink>
-                            </div>
-                            <div className="navigation__item">
-                                <NavLink to={"/comments"} activeStyle={{ fontWeight: 700 }}>Comments</NavLink>
-                            </div>
-                        </div>
+                        <Menu>
+                            <MenuItem to={"/counter"}>Counter</MenuItem>
+                            <MenuItem to={"/filter"}>Filter</MenuItem>
+                            <MenuItem to={"/articles"}>Articles</MenuItem>
+                            <MenuItem to={"/comments"}>Comments</MenuItem>
+                        </Menu>
                     </div>
                 </div>
                 <div className="app__main">
@@ -43,12 +37,12 @@ class App extends Component {
                             <Route path={"/articles"} component={ArticlesPage} />
                             <Route path={"/comments"} component={CommentsPage} />
                             <Route path={"/filter"} component={Select} />
-                            <Route path={"/error"} component={ErrorPage}/>
+                            <Route path={"/error"} component={ErrorPage} />
                             <Redirect from={"/"} to={"/articles"} />
                         </Switch>
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
